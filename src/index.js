e4f1adce6fdfb227d20642e37f50f135613433ed
furkaner <furkan08_@hotmail.com>
2022-03-08 17:29:35 +0300

    react-query provider added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries:{
+      refetchOnMount:false,
+      refetchOnWindowFocus:false,
+    }
+
+}});
 
 ReactDOM.render(
+  
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
