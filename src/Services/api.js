d30942f3d980e96c003447144e65b27682605ef7
furkaner <furkan08_@hotmail.com>
2022-03-08 17:32:00 +0300

    url changed

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export default class MealServices {
 
-   static async GetRandomMeal() {
+   static async GetRandomMeal(mealName) {
        console.log("calıstı")
-        const {data} =await  axios.get('https://www.themealdb.com/api/json/v1/1/random.php');
+        const {data} =await  axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`);
         return data;
 
     }
