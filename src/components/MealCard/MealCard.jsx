a7f206bc4c3e51ebee1ea5e263a32d36d948ac75
furkaner <furkan08_@hotmail.com>
2022-03-12 00:59:14 +0300

    class name changed

--- src/components/MealCard/MealCard.jsx
+++ src/components/MealCard/MealCard.jsx
@@ -4,7 +4,7 @@ import MealCardStyle from './MealCardStyle.css';
 
 const MealCard =({meal}) =>{
     return (
-        <div className="Container">
+        <div className="CardContainer">
            <img className="mealImage" src={meal?.strMealThumb} />
            <p className="mealName">{meal?.strMeal}</p>
         </div>
